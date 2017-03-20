491e7e276b5e15abc10702e7cf5387f657660250
vitor-vfm <vitor.vfm@gmail.com>
2017-03-19 19:58:17 -0600

    Two more data classes

---
+++ model/piece.rb
@@ -0,0 +1,22 @@
+class Piece
+
+    attr_accessor :category
+
+    def check_class_invariants
+        assert(@category, 'A piece\'s category cannot be nil')
+    end
+
+    def initialize_pre_cond
+    end
+
+    def initialize_post_cond
+    end
+
+    def initialize(category)
+        initialize_pre_cond
+        @category = category
+        initialize_post_cond
+        check_class_invariants
+    end
+
+end
---
+++ model/player.rb
@@ -0,0 +1,23 @@
+class Player
+
+    attr_accessor :category, :winning_pattern, :number
+
+    def check_class_invariants
+        assert(@category, 'Player must have a category')
+        assert(@winning_pattern, 'Player must have a winning pattern')
+    end
+
+    def initialize_pre_cond
+    end
+
+    def initialize_post_cond
+    end
+
+    def initialize(category, winning_pattern)
+        initialize_pre_cond
+        @category = category
+        @winning_pattern = winning_pattern
+        initialize_post_cond
+        check_class_invariants
+    end
+end

ad05a5bc818c896ff282c30b787fbe3f32c61e0b
vitor-vfm <vitor.vfm@gmail.com>
2017-03-19 19:57:16 -0600

    More interface to Board class. Should be done

--- model/board.rb
+++ model/board.rb
@@ -26,12 +26,41 @@ class Board
         check_class_invariants
     end
 
-    def add_piece_pre_cond(col)
-        assert(0 <= col && col < @n_cols)
+    def add_piece_pre_cond(col, piece)
+        assert(0 <= col && col < @n_cols, 'Column number is invalid')
+        assert(!column_full?(col), 'Column is full')
     end
 
-    def add_piece(col)
-
+    def add_piece_post_cond(col, piece)
+        assert(piece_in_column?(col, piece), 'The piece was not added to column')
     end
 
+    def add_piece(col, piece)
+        add_piece_pre_cond(col, piece)
+        # implement
+        add_piece_post_cond(col, piece)
+        check_class_invariants
+    end
+
+    def column_full?(col)
+        # implement
+    end
+
+    def piece_in_column?(col, piece)
+        # implement
+    end
+
+    def valid_columns_pre_cond
+    end
+
+    def valid_columns_post_cond
+    end
+
+    def valid_columns
+        valid_columns_pre_cond
+        # implement
+        res = [2,3,4]
+        valid_columns_post_cond
+        res
+    end
 end

2a3f2d8978117b245b9be48403592309ad5e838e
vitor-vfm <vitor.vfm@gmail.com>
2017-03-24 17:20:44 -0600

    Board class done

--- model/board.rb
+++ model/board.rb
@@ -23,7 +23,8 @@ class Board
 
         @n_rows = n_rows
         @n_cols = n_cols
-        @columns = @n_rows.times.map { Array.new(n_cols) }
+        @columns = @n_cols.times.map { Array.new(n_rows) }
+        @diagonal_indices = diagonal_indices(n_rows, n_cols)
 
         initialize_post_cond
         check_class_invariants
@@ -88,10 +89,10 @@ class Board
         check_class_invariants
 
         # returns the positions of the pattern or nil
-        pos = positions
+        rows = positions
 
         # find rows with the pattern
-        pos.each_with_index do |row, row_index|
+        rows.each_with_index do |row, row_index|
             pattern_position = find_linear_pattern(row, pattern)
             if pattern_position
                 return pattern_position.map{|i| [row_index, i]}
@@ -99,17 +100,46 @@ class Board
         end
 
         # find columns with the pattern
-        pos.transpose.each_with_index do |col, col_index|
+        cols = rows.transpose
+        cols.each_with_index do |col, col_index|
             pattern_position = find_linear_pattern(col, pattern)
             if pattern_position
                 return pattern_position.map{|i| [i, col_index]}
             end
         end
+
+        # find diagonals with the pattern
+        @diagonal_indices.select {|d| d.length >= pattern.length}.each do |diagonal_index|
+            diagonal = diagonal_index.map {|i,j| rows[i][j]}
+            pattern_position = find_linear_pattern(diagonal, pattern)
+            if pattern_position
+                return pattern_position.map{|i| diagonal_index[i]}
+            end
+        end
+
         pattern_found_post_cond
+        nil
     end
 
     def find_linear_pattern(line, pattern)
         pattern_index = line.each_cons(pattern.length).to_a.index(pattern)
         pattern_index ? pattern.length.times.map{|i| pattern_index + i} : nil
+    end
+
+    def diagonal_indices(n_rows, n_cols)
+        # returns the indices of the diagonals of a matrix of size (n_rows x n_cols),
+        # as a collection of arrays of pairs (i,j)
+        #
+        # A square matrix 2x2 would have diagonal_indices
+        # [ [1,0] ],
+        # [ [0,0],[1,1] ],
+        # [ [0,1] ],
+        # [ [0,0] ],
+        # [ [0,1], [1,0]],
+        # [ [1,1] ]
+        all_index_pairs = n_rows.times.flat_map {|r| n_cols.times.map{ |c| [r,c]}}
+        left_right = all_index_pairs.group_by{|i,j| i - j}.values
+        right_left = all_index_pairs.group_by{|i,j| i + j}.values
+        left_right + right_left
     end
 end
--- test/test_model.rb
+++ test/test_model.rb
@@ -1,5 +1,7 @@
 #! /usr/bin/env ruby
 
+require 'set'
+
 require_relative '../model/board'
 require_relative '../model/otto_n_toot'
 require_relative '../model/connect4'
@@ -7,22 +9,56 @@ require_relative '../model/connect4'
 class TestModel
 
     def self.test_board
-        b = Board.new(8,7)
-        b.add_piece(2, :T)
+        n_rows = 8
+        n_cols = 7
+        b = Board.new(n_rows,n_cols)
+        # Board goal:
+        #          :F
+        #          :F
+        #          :F
+        #          :F
+        #       :T :F
+        #       :O :F
+        #    :O :T :F
+        # :O :T :O :F
+
+        b.add_piece(3, :F)
+        b.add_piece(3, :F)
+        b.add_piece(3, :F)
+        b.add_piece(3, :F)
+        b.add_piece(3, :F)
+        b.add_piece(3, :F)
+        b.add_piece(3, :F)
+        b.add_piece(3, :F)
+
         b.add_piece(2, :O)
         b.add_piece(2, :T)
         b.add_piece(2, :O)
         b.add_piece(2, :T)
-        b.add_piece(2, :O)
-        b.add_piece(2, :T)
+
         b.add_piece(1, :T)
         b.add_piece(0, :O)
+        b.add_piece(1, :O)
+
         pos = b.positions
         cols = b.valid_columns
+        assert(cols == (n_cols.times.to_a - [3]), 'valid_columns doesn\'t work')
+
         pat = b.pattern_found([:T,:T,:T,:T])
         pat2 = b.pattern_found([:T,:O,:T,:O])
-        pat3 = b.pattern_found([:T,:T])
-        puts cols, pos
+        pat3 = b.pattern_found([:O,:O,:O])
+        pat4 = b.pattern_found([:O,:O])
+        puts pos, cols, pat, pat2, pat3, pat4
+
+        t = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8],
+            [9,10,11,12]
+        ]
+        expected = Set.new([[9],[5,10],[1,6,11],[2,7,12],[3,8],[4],[1],[2,5],[3,6,9],[4,7,10],[8,11],[12]])
+        idx = b.diagonal_indices(3,4)
+        result = Set.new(idx.map {|d| d.map {|i,j| t[i][j]}})
+        assert(result == expected, 'diagonal calculation is incorrect')
     end
 
     def self.test_game
@@ -35,4 +71,4 @@ class TestModel
 end
 
 TestModel.test_board
-# TestModel.test_game
+TestModel.test_game

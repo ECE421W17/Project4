d38961d2f763425319a0f2956152c411f1c0accd
adlawren <adlawren010@gmail.com>
2017-03-28 10:53:45 -0600

    WIP

--- test.rb
+++ test.rb
@@ -1,20 +1,73 @@
 require 'gtk2'
 
-# Gtk.init
-window = Gtk::Window.new("My ruby-gnome2 project")
+class TestView
+    def init
+        puts 'In init'
+        @command_string = nil
+        @output = nil
 
-button = Gtk::Button.new("Hello World")
-button.signal_connect("clicked") { 
-    puts "HelloWorld"
-}
+        # .... -_-
+        @output_label = nil 
+    end
 
-window.signal_connect("destroy") {
-    puts "destroy event occurred"
-    Gtk.main_quit
-}
+    def run
+        Gtk.init
 
-window.border_width = 10
-window.add(button)
-window.show_all
+        # ----- Window -----
+        window = Gtk::Window.new("My ruby-gnome2 project")
 
-Gtk.main
+        window.signal_connect("destroy") {
+            puts "destroy event occurred"
+            Gtk.main_quit
+        }
+
+        window.border_width = 10
+        # -----
+
+
+        # ----- Text input -----
+        entry = Gtk::Entry.new()
+        entry.signal_connect("insert-text") do |entry, new_character|
+            # puts "New text: #{entry.text}#{new_character}"
+            @command_string = "#{entry.text}#{new_character}"
+        end
+        # -----
+
+        # ----- Button -----
+        button = Gtk::Button.new("Hello World")
+        button.signal_connect("clicked") { 
+            puts "HelloWorld"
+            puts "Command #{@command_string}"
+
+            # TODO: Run command
+            # @output_label.text = "Updated"
+
+            Thread.new do
+                @output = `#{@command_string}`
+                puts @output
+
+                @output_label.text = @output
+            end
+        }
+        # -----
+
+        # ----- Label -----
+        # label = Gtk::Label.new
+        @output_label = Gtk::Label.new
+        @output_label.text = "Test test test"
+        # -----
+
+        vbox = Gtk::VBox.new()
+        vbox.pack_start entry, :expand => false, :fill => false, :padding => 0
+        vbox.pack_start button, :expand => false, :fill => false, :padding => 0
+        vbox.pack_start @output_label, :expand => false, :fill => false, :padding => 0
+
+        window.add(vbox)
+        window.show_all 
+
+        Gtk.main
+    end
+end
+
+tv = TestView.new
+tv.run
